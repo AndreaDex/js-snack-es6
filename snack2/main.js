cb8e46e9a4cfa0c1544090432a40ecc5b032455d
AndreaDex <desiderio.andrea@outlook.it>
2021-05-11 16:20:03 +0200

    snack2 completo

--- snack2/main.js
+++ snack2/main.js
@@ -7,15 +7,14 @@ Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli su
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
 // ! Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
-// ! Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 
 
-/* const squadra = {
-    nome : "Samb",
-    chiavePunti,
-    chiaveFalli,
-}
-console.log(squadra); */
+
+/* let punti = getRndInteger(0, 90);
+let falli = getRndInteger(0, 59);
+const  puntiFatti = punti;
+const  falliSubiti = falli;
+ */
 
 const teams = [
     {
@@ -50,18 +49,29 @@ const teams = [
     }
 ]
 
+// ! Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-let punti = getRndInteger(0, 90);
-let falli = getRndInteger(0, 59);
-const  puntiFatti = punti;
-const  falliSubiti = falli;
+for (let index = 0; index < teams.length; index++) {
+    const element = teams[index];
+    element.puntiFatti = getRndInteger(0, 90);
+    element.falliSubiti = getRndInteger(0, 59)
+    
+}
+//console.log(teams);
 
-console.log(teams);
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+//let nomiEfalli ;
+var nomiEpunti = []
+for (let index = 0; index < teams.length; index++) {
+    const element = teams[index];
+    var {nome, falliSubiti} = element ;
+    nomiEpunti.push(nome, falliSubiti)
+    
+}
+console.log(nomiEpunti);
 
-const [primo] = teams;
-console.log(primo);
 
